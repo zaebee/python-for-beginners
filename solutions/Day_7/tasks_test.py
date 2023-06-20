16a1fffdda9a1e1e045ef6906960c0e4cc438257
Andrei Gromov <zaebuntu@gmail.com>
2023-06-20 11:28:21 +0200

    added testcases for Day_7 and Day_8

---
+++ solutions/Day_7/tasks_test.py
@@ -0,0 +1,14 @@
+"""unittests for Day_7."""
+
+import tasks
+import unittest
+
+
+class TasksTest(unittest.TestCase):
+
+  def test_give_me_a_name(self):
+    pass
+
+
+if __name__ == '__main__':
+  unittest.main()
