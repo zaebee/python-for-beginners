888282c00e03608e4794efd09291933e2d9c9b20
Andrei Gromov <zaebuntu@gmail.com>
2023-06-20 10:24:10 +0200

    added testcases for Day_5

---
+++ solutions/Day_5/tasks.py
@@ -0,0 +1,25 @@
+def square_odds(n: str) -> str:
+  """Use a list comprehension to square each odd number in a list.
+  The list is input by a sequence of comma-separated numbers."""
+
+  return ''
+
+
+def transactions(data: str) -> int:
+  """Write a program that computes the net amount of a bank account based
+  a transaction log from console input.
+  The transaction log format is shown as following:
+  D 100
+  W 200
+  D means deposit while W means withdrawal.
+  Suppose the following input is supplied to the program:
+
+  D 300
+  D 300
+  W 200
+  D 100
+  Then, the output should be:
+
+  500
+  """
+  return 0
---
+++ solutions/Day_5/tasks_test.py
@@ -0,0 +1,27 @@
+import tasks
+import unittest
+
+
+class TasksTest(unittest.TestCase):
+
+  def test_square_odds(self):
+    actual = tasks.square_odds('1,2,3,4,5,6,7,8,9')
+
+    expected = '1,9,25,49,81'
+    self.assertEqual(actual, expected)
+
+  def test_transactions(self):
+    data = '''
+    D 300
+    D 300
+    W 200
+    D 100
+    '''
+    actual = tasks.transactions(data)
+
+    expected = 500
+    self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+  unittest.main()
