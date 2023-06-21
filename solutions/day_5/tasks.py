86c0320ee19f1a36baf2d3d0ace761f4bce8de88
Andrei Gromov <zaebuntu@gmail.com>
2023-06-21 12:54:34 +0200

    Renamed solution folders to follow style guide

--- solutions/day_5/tasks.py
+++ solutions/day_5/tasks.py
@@ -1,11 +1,13 @@
-def square_odds(n: str) -> str:
+"""Tasks for Day_5."""
+
+def square_odds(_: str) -> str:
   """Use a list comprehension to square each odd number in a list.
   The list is input by a sequence of comma-separated numbers."""
 
   return ''
 
 
-def transactions(data: str) -> int:
+def transactions(_: str) -> int:
   """Write a program that computes the net amount of a bank account based
   a transaction log from console input.
   The transaction log format is shown as following:
--- solutions/day_5/tasks_test.py
+++ solutions/day_5/tasks_test.py
@@ -7,18 +7,21 @@ from . import tasks
 
 
 class TasksTest(unittest.TestCase):
+  """Tests for tasks."""
 
   @parameterized.expand([
       ('positive', '1,2,3,4,5,6,7,8,9', '1,9,25,49,81'),
       ('negative', '-3,-2,-1,0', '9,1'),
       ('with_spaces', '-3, 1, 5,  7,4', '9,1,25,49'),
   ])
-  def test_square_odds(self, name, test_input, expected):
+  def test_square_odds(self, _, test_input, expected):
+    """Checks square for odds numbers."""
     actual = tasks.square_odds(test_input)
 
     self.assertEqual(actual, expected)
 
   def test_transactions(self):
+    """Calculates amount after all transactions."""
     data = '''
     D 300
     D 300
