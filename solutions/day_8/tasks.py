2a26e9ce5fee37f0704bb57bf9a8fb299bf7a2bc
vo1ha <136185026+vo1ha@users.noreply.github.com>
2023-07-26 23:36:41 +0200

    Question 2 added, code reformatted

--- solutions/day_8/tasks.py
+++ solutions/day_8/tasks.py
@@ -13,7 +13,7 @@ def compute_the_frequency(input_string):
     for i in input_list:
         # Count each value in a list and concatenate the result with the value itself.
         # Add concatenated string to a set
-        output_set.add(i+':'+str(input_list.count(i)))
+        output_set.add(i + ':' + str(input_list.count(i)))
 
     # Convert set to a list to sort items
     output_list = list(output_set)
@@ -27,13 +27,16 @@ def compute_the_frequency(input_string):
     Write a method which can calculate square value of number.
     Using the ** operator which can be written as n**p where means n^p"""
 
+
 class MathOperations:
     """ Class for math operations"""
+
     def __init__(self, input_int):
         self.input_int = input_int
+
     def square_value(self):
         """ Return the square value of number"""
-        return self.input_int**2
+        return self.input_int ** 2
 
 
 """ Question 24
@@ -42,22 +45,26 @@ class MathOperations:
     And add document for your own function.
     The built-in document method is __doc__"""
 
+
 def return_documentation(input_func):
     return input_func.__doc__
 
 
 """ Question 25
     Define a class, which have a class parameter and have a same instance parameter.
-    Hint: Define an instance parameter, need add it in __init__ method. 
+    Hint: Define an instance parameter, need to add it in __init__ method. 
     You can init an object with construct parameter or set the value later
     """
-class StringOperations:
-    """ Class for string operations"""
-    def __init__(self, input_string):
-        self.input_string = input_string
-    def upper_string(self, input_string):
-        """ Return the square value of number"""
-        return input_string.upper()
 
-test_object = StringOperations()
-print(test_object.upper_string('test'))
+
+class ClassAndInstanceParameters:
+    """ Class for string operations"""
+    input_string = 'replace with your string'
+
+    # Set default value for input_string, so you can initiate an object without this parameter
+    def __init__(self, input_string=input_string):
+        self.input_string = input_string
+
+    def set_value(self, new_string):
+        """ Method to set a new value for input_string"""
+        self.input_string = new_string
--- solutions/day_8/tasks_test.py
+++ solutions/day_8/tasks_test.py
@@ -5,14 +5,14 @@ from . import tasks
 
 
 class TasksTest(unittest.TestCase):
-  """Unit tests for tasks."""
+    """Unit tests for tasks."""
 
-  def test_compute_the_frequency(self):
-    """Returns sorted frequency of the words from the input string"""
-    given = '''New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.'''
+    def test_compute_the_frequency(self):
+        """Returns sorted frequency of the words from the input string"""
+        given = '''New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.'''
 
-    actual = tasks.compute_the_frequency(given)
-    expected = '''2:2
+        actual = tasks.compute_the_frequency(given)
+        expected = '''2:2
 3.:1
 3?:1
 New:1
@@ -24,36 +24,55 @@ choosing:1
 or:2
 to:1'''
 
-    self.assertEqual(actual, expected)
+        self.assertEqual(actual, expected)
 
-  def test_square_value(self):
-    """Returns squared values."""
-    given = 11
-    test_object = tasks.MathOperations(given)
-    actual = test_object.square_value()
-    expected = 121
+    def test_square_value(self):
+        """Returns squared values."""
+        given = 11
+        test_object = tasks.MathOperations(given)
+        actual = test_object.square_value()
+        expected = 121
 
-    self.assertEqual(actual, expected)
+        self.assertEqual(actual, expected)
 
-  def test_return_abs_documentation(self):
-    """Returns documentation for a function."""
-    actual = tasks.return_documentation(abs)
-    self.assertIsNotNone(actual)
+    def test_return_abs_documentation(self):
+        """Returns documentation for a function."""
+        actual = tasks.return_documentation(abs)
+        self.assertIsNotNone(actual)
 
-  def test_return_int_documentation(self):
-    """Returns documentation for a function."""
-    actual = tasks.return_documentation(int)
-    self.assertIsNotNone(actual)
+    def test_return_int_documentation(self):
+        """Returns documentation for a function."""
+        actual = tasks.return_documentation(int)
+        self.assertIsNotNone(actual)
 
-  def test_return_input_documentation(self):
-    """Returns documentation for a function."""
-    actual = tasks.return_documentation(input)
-    self.assertIsNotNone(actual)
+    def test_return_input_documentation(self):
+        """Returns documentation for a function."""
+        actual = tasks.return_documentation(input)
+        self.assertIsNotNone(actual)
 
-  def test_return_compute_the_frequency_documentation(self):
-    """Returns documentation for a function."""
-    actual = tasks.return_documentation(tasks.compute_the_frequency)
-    self.assertIsNotNone(actual)
+    def test_return_compute_the_frequency_documentation(self):
+        """Returns documentation for a function."""
+        actual = tasks.return_documentation(tasks.compute_the_frequency)
+        self.assertIsNotNone(actual)
+
+    def test_class_parameters(self):
+        test_object = tasks.ClassAndInstanceParameters()
+        actual = test_object.input_string
+        expected = 'replace with your string'
+        self.assertEqual(actual, expected)
+
+    def test_instance_parameters(self):
+        test_object = tasks.ClassAndInstanceParameters('my string')
+        actual = test_object.input_string
+        expected = 'my string'
+        self.assertEqual(actual, expected)
+
+    def test_new_set_parameters(self):
+        test_object = tasks.ClassAndInstanceParameters('my string')
+        test_object.set_value('my new string')
+        actual = test_object.input_string
+        expected = 'my new string'
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
